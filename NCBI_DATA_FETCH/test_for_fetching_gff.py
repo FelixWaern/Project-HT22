47b07ec4d745e531d87c06b862adae9efed38722
Felix <Felix.wae@gmail.com>
2022-11-09 16:36:49 +0100

    Created an draft for a fetch
    A draft fetch testing different methods. It works to an degree now.

---
+++ NCBI_DATA_FETCH/test_for_fetching_gff.py
@@ -0,0 +1,27 @@
+# For testing the Bio.Entrez package
+#!!!! Remember to create check for packages and correct API-key 
+
+print("Starting Entrez id fetch test")
+
+from Bio import Entrez
+
+
+#API-KEY for Felix.wae@gmail.com NCBI account: "7b4a5e9841f79495be73767323ad485fda08"
+
+Entrez.email = "Felix.wae@gmail.com"
+Entrez.api_key = "7b4a5e9841f79495be73767323ad485fda08"
+
+handle = Entrez.efetch(db="nuccore", id="NC_000913.3", rettype="fasta_cds_aa", retmode="text")
+info = handle.read()
+print(type(info))
+print(info)
+#print(handle.readline().strip())
+#ft och fasta_cds_na var helt ok. gbwithparts var alldelles för stor. 
+# fasta_cds_aa works. 
+
+
+handle.close()
+print("Finished")
+
+
+
