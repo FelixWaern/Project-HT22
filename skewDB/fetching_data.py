b6fdf1e345bdff91166a84e8521fa05beabb262a
AshwiniJatti <ashwini.jatti@gmail.com>
2022-11-10 14:53:44 +0100

    Update fetching_data.py

--- skewDB/fetching_data.py
+++ skewDB/fetching_data.py
@@ -4,6 +4,7 @@ import pandas as pd
 df = pd.read_csv('https://skewdb.org/view/gcskewdb.csv')
 tot_size = len(df.index)
 print(tot_size)
+print(df.loc[df["realm1"] == "Bacteria"])
 # storing parameter values in variable
 params = df.loc[df["name"] == "NC_000913.3",["siz","div","shift"]]
 print(params)
