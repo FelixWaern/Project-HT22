5090b95ce03172eb19484a24509f76036c092307
AshwiniJatti <ashwini.jatti@gmail.com>
2022-11-10 14:34:47 +0100

    fetching parameter values

---
+++ skewDB/fetching_data.py
@@ -0,0 +1,9 @@
+# first import the module
+import pandas as pd
+# reading csv file into dataframe
+df = pd.read_csv('https://skewdb.org/view/gcskewdb.csv')
+tot_size = len(df.index)
+print(tot_size)
+# storing parameter values in variable
+params = df.loc[df["name"] == "NC_000913.3",["siz","div","shift"]]
+print(params)
