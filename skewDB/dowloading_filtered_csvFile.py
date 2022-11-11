5f84222a3011705ab62fd2f36221308e8ecc1892
AshwiniJatti <ashwini.jatti@gmail.com>
2022-11-11 16:18:10 +0100

    edited the fetching_data script and added new script file to download the csv file

---
+++ skewDB/dowloading_filtered_csvFile.py
@@ -0,0 +1,34 @@
+# first import the module
+import pandas as pd
+
+# reading csv file into dataframe
+df = pd.read_csv('https://skewdb.org/view/gcskewdb.csv')
+
+# finding the total size of the dataframe
+tot_size = len(df.index)
+print(tot_size)
+
+#filtering for bacteria. removing archea and other stuffs
+print(df["realm1"].unique())
+df_1 = df.loc[df["realm1"] == "Bacteria"]
+df1_size = len(df_1.index)
+print(df1_size)
+
+#checking for plasmid DNA
+print(df_1["plasmid"].unique())
+df_2 = df_1.loc[df_1["plasmid"] == 0]
+df2_size = len(df_2.index)
+print(df2_size)
+
+# Filtering for rmsGC < 0.2
+df_3 = df_2.loc[df_2["rmsGC"] < 0.2]
+df3_size = len(df_3.index)
+print(df3_size)
+
+# Filtering for div value. 
+df_4 = df_3.loc[df_3["div"].between(0.3333,0.6666)]
+df4_size = len(df_4.index)
+print(df4_size)
+
+#Dowloading the filtered dataframe to local computer
+df_4.to_csv('C:/Ashwini/Applied bioinformatics/FilteredFile.csv')
--- skewDB/fetching_data.py
+++ skewDB/fetching_data.py
@@ -1,37 +1,16 @@
-# first import the module
+#import module
 import pandas as pd
 
-# reading csv file into dataframe
-df = pd.read_csv('https://skewdb.org/view/gcskewdb.csv')
-
-# finding the total size of the dataframe
-tot_size = len(df.index)
-print(tot_size)
-
-#filtering for bacteria. removing archea and other stuffs
-print(df["realm1"].unique())
-df_1 = df.loc[df["realm1"] == "Bacteria"]
-df1_size = len(df_1.index)
-print(df1_size)
-
-#checking for plasmid DNA
-print(df_1["plasmid"].unique())
-df_2 = df_1.loc[df_1["plasmid"] == 0]
-df2_size = len(df_2.index)
-print(df2_size)
-
-# Filtering for rmsGC < 0.2
-df_3 = df_2.loc[df_2["rmsGC"] < 0.2]
-df3_size = len(df_3.index)
-print(df3_size)
+# Reading filtered csv file
+df = pd.read_csv('C:/Ashwini/Applied bioinformatics/FilteredFile.csv')
 
 # storing parameter values in variable for e coli and bacillus subtilis
-E_coli = df_3.loc[df["name"] == "NC_000913.3",["siz","div","shift","rmsGC"]]
+E_coli = df.loc[df["name"] == "NC_000913.3",["siz","div","shift","rmsGC"]]
 print(E_coli)
-B_subtilis = df_3.loc[df["name"] == "NC_000964.3",["siz","div","shift","rmsGC"]]
+B_subtilis = df.loc[df["name"] == "NC_000964.3",["siz","div","shift","rmsGC"]]
 print(B_subtilis)
 
-print(df_3.loc[df["name"] == "NZ_CP071399.1",["realm1","plasmid","siz","div","shift","rmsGC"]])
+print(df.loc[df["name"] == "NZ_CP071399.1",["realm1","plasmid","siz","div","shift","rmsGC"]])
 
 # Finding ori and terminus
 #print(E_coli["siz"])
