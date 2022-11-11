397ff3504bcc95c6c2aed57b6c9cf9c0c09005d8
Felix <Felix.wae@gmail.com>
2022-11-11 11:55:58 +0100

    testing some parsing with fetch
    Trying out both in combination

--- NCBI_DATA_FETCH/test_for_fetching_gff.py
+++ NCBI_DATA_FETCH/test_for_fetching_gff.py
@@ -11,13 +11,15 @@ from Bio import Entrez
 Entrez.email = "Felix.wae@gmail.com"
 Entrez.api_key = "7b4a5e9841f79495be73767323ad485fda08"
 
-handle = Entrez.efetch(db="nuccore", id="NC_000913.3", rettype="fasta_cds_aa", retmode="text")
-info = handle.read()
-print(type(info))
-print(info)
+handle = Entrez.efetch(db="nuccore", id="NC_000913.3", rettype="gb", retmode="text")
+
+#info = handle.read()
+#print(type(info))
+#print(info)
 #print(handle.readline().strip())
 #ft och fasta_cds_na var helt ok. gbwithparts var alldelles för stor. 
 # fasta_cds_aa works. 
+#Vi ska hämta genbank file som en handle
 
 
 handle.close()
