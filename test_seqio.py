b85969e2dcf713fc11dd4ff410b71935c72b5874
lindbergsara <82642085+lindbergsara@users.noreply.github.com>
2022-11-14 13:36:56 +0100

    Fetch all rrna products

--- test_seqio.py
+++ test_seqio.py
@@ -1,7 +1,20 @@
 from Bio import SeqIO
+from Bio import SeqFeature   
 
 with open("GCF_000009045.1_ASM904v1_genomic.gbff") as input_handle:
     for record in SeqIO.parse(input_handle, "genbank"):
         print(record.id)
         print(repr(record.seq))
         print(len(record))
+
+    for index, record in enumerate(SeqIO.parse("GCF_000009045.1_ASM904v1_genomic.gbff", "genbank")):
+        print(
+            "index %i, ID = %s, length %i, with %i features"
+            % (index, record.id, len(record.seq), len(record.features))
+        )
+        print(record)
+
+        for feature in record.features:
+            if feature.type == "rRNA":
+                print(feature.qualifiers.get("product"), feature.location)
+
