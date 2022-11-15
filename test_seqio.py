4b3d04abd09e7015a1407b6a5457eac1d45eb7e6
lindbergsara <82642085+lindbergsara@users.noreply.github.com>
2022-11-15 10:30:29 +0100

    imported time module

--- test_seqio.py
+++ test_seqio.py
@@ -1,17 +1,18 @@
 from Bio import SeqIO
-from Bio import SeqFeature   
+from Bio import SeqFeature 
+import time  
 
+t0 = time.time()
 with open("GCF_000006765.1_ASM676v1_genomic.gbff") as input_handle:
     for record in SeqIO.parse(input_handle, "genbank"):
         print(record.id)
         print(repr(record.seq))
         print(len(record))
-
-    #for index, record in enumerate(SeqIO.parse("GCF_000009045.1_ASM904v1_genomic.gbff", "genbank")):
-     #   print(
-      #      "index %i, ID = %s, length %i, with %i features"
-       #     % (index, record.id, len(record.seq), len(record.features))
-        #)
+    for index, record in enumerate(SeqIO.parse("GCF_000009045.1_ASM904v1_genomic.gbff", "genbank")):
+        print(
+            "index %i, ID = %s, length %i, with %i features"
+            % (index, record.id, len(record.seq), len(record.features))
+        )
         #print(record)
 
     for feature in record.features:
@@ -19,3 +20,7 @@ with open("GCF_000006765.1_ASM676v1_genomic.gbff") as input_handle:
             for product in feature.qualifiers.get("product"):
                 if "16S" in product:
                     print(feature.qualifiers.get("product"), feature.location)
+
+t1 = time.time()
+total = t1-t0
+print(total)
