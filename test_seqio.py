17fd7002ee83b37a2253aae7655392b3f45b775d
lindbergsara <82642085+lindbergsara@users.noreply.github.com>
2022-11-15 15:01:54 +0100

    fixed warning messages

--- test_seqio.py
+++ test_seqio.py
@@ -1,26 +1,33 @@
 from Bio import SeqIO
-from Bio import SeqFeature 
 import time  
+import warnings
 
 t0 = time.time()
 with open("GCF_000006765.1_ASM676v1_genomic.gbff") as input_handle:
-    for record in SeqIO.parse(input_handle, "genbank"):
-        print(record.id)
-        print(repr(record.seq))
-        print(len(record))
-    for index, record in enumerate(SeqIO.parse("GCF_000009045.1_ASM904v1_genomic.gbff", "genbank")):
+    for index, record in enumerate(SeqIO.parse(input_handle, "genbank")):
         print(
-            "index %i, ID = %s, length %i, with %i features"
+            "CHROMOSOME: index %i, ID = %s, length %i, with %i features"
             % (index, record.id, len(record.seq), len(record.features))
         )
         #print(record)
 
+    temp = []
     for feature in record.features:
         if feature.type == "rRNA":
             for product in feature.qualifiers.get("product"):
                 if "16S" in product:
                     print(feature.qualifiers.get("product"), feature.location)
+                    temp.append(str(feature.location))
+                    
+    count = 0
+    if len(temp) == 0:
+        count += 1
+        print("---------- WARNING ----------")
+        warnings.warn(f" \n No rRNA 16S was found for {record.id}") 
+    
 
 t1 = time.time()
 total = t1-t0
-print(total)
+print("---------- SUMMARY ----------")
+print("Elapsed time:", total)
+print("Number of warnings:", count)
