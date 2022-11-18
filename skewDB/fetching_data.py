6e0cbba5c6d64851957bc827ae12ca619730424b
AshwiniJatti <ashwini.jatti@gmail.com>
2022-11-18 20:58:02 +0100

    updated fetching_data.py

--- skewDB/fetching_data.py
+++ skewDB/fetching_data.py
@@ -9,6 +9,7 @@ df = pd.read_csv('C:/Ashwini/Applied bioinformatics/FilteredDataFile.csv')
 ter = []
 ori = []
 
+#Method defined for calculating the origin of replication based on the positive/negative value of shift
 def shift_value(shift, siz):
     if shift > 0:
         origin = shift
@@ -18,17 +19,30 @@ def shift_value(shift, siz):
         origin = 1
     return origin
 
+#Method defined for calculate the size after adding the shift value 
+def shift_value_terminus(shift, siz):
+    if shift > 0:
+        new_siz = siz + shift
+    elif shift < 0:
+        new_siz = siz + shift
+    else:
+         new_siz = siz
+    return new_siz
+
+#For loop for iterating over all chromosomes
 for i in range(len(df)):
-    terminal = df.loc[i, "siz"] * df.loc[i, "div"]
-    rounded_terminal = round(terminal)
-    ter.append(rounded_terminal)
     shift = df.loc[i, "shift"]
     siz = df.loc[i, "siz"]
     origin = shift_value(shift, siz)
     ori.append(origin)
-
-df["Ter"] = ter
+    new_siz = shift_value_terminus(shift, siz)
+    terminus = new_siz * df.loc[i, "div"]
+    rounded_terminus = round(terminus)
+    ter.append(rounded_terminus)
+    
+#assigning ori and ter lists to dataframe
 df["Ori"] = ori
+df["Ter"] = ter
 
 # checking the calculation for the selected bacterias
 E_coli = df.loc[df["name"] == "NC_000913.3",["dnaApos","siz","div","shift","Ori", "Ter"]]
@@ -41,4 +55,4 @@ print(P_aeruginosa)
 df = df.loc[:, ~df.columns.str.contains('^Unnamed')]
 
 #writing to csv file stored in local computer
-#df.to_csv("C:/Ashwini/Applied bioinformatics/dataFile_with_ori&ter.csv")
+df.to_csv("C:/Ashwini/Applied bioinformatics/File_with_ori&ter.csv")
--- skewDB/gcfit_file.py
+++ skewDB/gcfit_file.py
@@ -1,19 +1,24 @@
 # Import libraries
 import glob
 import pandas as pd
+import numpy as np
 
 # Get CSV files list from a folder
 path = "C:/Ashwini/Applied bioinformatics/gcfits"
 csv_files = glob.glob(path + "/*.csv")
 #print(csv_files)
+s = csv_files[1]
+st = s.replace("C:/Ashwini/Applied bioinformatics/gcfits", "")
+print(st)
 
 # Read each CSV file into DataFrame
 # This creates a list of dataframes
 df_list = (pd.read_csv(file) for file in csv_files)
 
 
+
 # Concatenate all DataFrames
-big_df   = pd.concat(df_list, ignore_index=True)
-print("hi")
+#big_df   = pd.concat(df_list, ignore_index=True)
+
 #print(big_df[1,1])
 #print(len(big_df.index))
