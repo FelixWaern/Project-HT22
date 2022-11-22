31fd18f9c0cce1517c5ed3e87b96e3a1d4078d1d
FelixWaern <98013293+FelixWaern@users.noreply.github.com>
2022-11-22 14:34:28 +0100

    fixed logging

--- start.py
+++ start.py
@@ -1,5 +1,5 @@
 # The top script which calls and checks 
-#TODO Email NCBI about the faulty files, 
+#TODO Email NCBI about the faulty files
 
 
 import sys
@@ -14,19 +14,11 @@ from skewDB import dowloading_filtered_csvFile as download_filtered
 def start(csv_path, email, api_key, local_storage_path):
     # Start logging
     if __name__ == "__main__":
-        logging.basicConfig(level=logging.DEBUG, filename="faulty_NCBI_records", filemode="a+",
-                        format="%(asctime)-15s %(levelname)-8s %(message)s")
-                        
-        """Function that searches for 16S rRNA genes"""
-        # Get start date and time of analysis
         now = datetime.datetime.now()
         start_datetime = now.strftime('%Y-%m-%d %H:%M')
-
-        # Create log file
-        root_logger = logging.getLogger()
-        root_logger.setLevel(logging.DEBUG)
-        handler = logging.FileHandler(f'warnings_rrna_{start_datetime}.log', 'w', 'utf-8')
-        root_logger.addHandler(handler)
+        logging.basicConfig(level=logging.DEBUG, filename="logfile_"+ start_datetime, filemode="a+",
+                        format="%(asctime)-15s %(levelname)-8s %(message)s")
+                        
     
 
     #Check if csv is downloaded & filtered 
