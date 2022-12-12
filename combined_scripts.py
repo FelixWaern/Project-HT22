bcc02edc4a80ff4b979c12235fd341e63dbdba15
FelixWaern <98013293+FelixWaern@users.noreply.github.com>
2022-12-12 10:20:31 +0100

    not much

--- combined_scripts.py
+++ combined_scripts.py
@@ -11,7 +11,7 @@ def get_rRNA_intervals(csv_path, email, api_key, local_storage_path, a_list, ver
     org_df = fd.fetch_csv_as_df(csv_path) 
     #Ta från [22000:23000]
     #test_df = org_df.iloc[900:1000] # [700:800] Did not work :/ 
-    test_df = org_df.head(10000) 
+    test_df = org_df.head(12000)
 
     if a_list != None:
         df = org_df.loc[org_df['name'].isin(a_list)]
