e5c9779b18944aff96a550b8d8cf89906eb8fbbe
lindbergsara <82642085+lindbergsara@users.noreply.github.com>
2022-12-19 14:59:00 +0100

    error in data from ncbi. fixed regular expression to fit that case

--- rrna_leading_lagging.py
+++ rrna_leading_lagging.py
@@ -119,15 +119,15 @@ def rrna_lead_lag(csv_path, rrna_locus_list):
             ter = (df_rrna_ori_ter.loc[row, "siz"] * df_rrna_ori_ter.loc[row, "div"]) + df_rrna_ori_ter.loc[row, "shift"] + 1
             # find the first/last position of the rrna gene with regex
             rrna = re.findall(r'(?<=\[)[0-9]+', str(df_rrna_ori_ter.loc[row, col]))
-            rrna_comp = re.findall(r'(?<=\:)[0-9]+', str(df_rrna_ori_ter.loc[row, col]))
+            rrna_comp = re.findall(r'(?<=\:|\>)[0-9]+', str(df_rrna_ori_ter.loc[row, col]))
             # rrna in this column
             if rrna:
                 count_rrna += 1
                 nr_rrna += 1
                 df_rrna_csv.loc[count_rrna, "name"] = acc_nr
                 #print(df_locus.loc[row, col])
-                df_rrna_csv.loc[count_rrna, "locus_tag"] = df_locus.loc[row, col]
-                #df_rrna_csv.loc[count_rrna, "locus_tag"] = df_locus.loc[row, col][2:-2]
+                #df_rrna_csv.loc[count_rrna, "locus_tag"] = df_locus.loc[row, col]
+                df_rrna_csv.loc[count_rrna, "locus_tag"] = df_locus.loc[row, col][2:-2]
                 df_rrna_csv.loc[count_rrna, "start"] = rrna[0]
                 df_rrna_csv.loc[count_rrna, "stop"] = rrna_comp[0]
             # no rrna in this column
